c3d4c073828c008fcca88dd627d28bd7fc80f6be
Ebihara Yusuke <yusuke@ebihara.me>
2023-03-10 03:43:02 +0900

    コースの自動認識

---
+++ capture_tracks.py
@@ -0,0 +1,31 @@
+import datetime
+
+import device
+import cv2
+
+print(device.getDeviceList())
+
+# カメラをオープンする
+capture = cv2.VideoCapture(0)
+
+# カメラがオープン出来たか？
+camera_opened = capture.isOpened()
+
+track_id = 1
+while camera_opened:
+
+    # フレーム画像の取得
+    ret, frame = capture.read()
+    
+    # 画像の表示
+    cv2.imshow("Image", frame)
+
+    if cv2.waitKey(1) != -1:
+        # キー入力で終了
+        cv2.imwrite(f"tracks/{track_id}.png", frame)
+        track_id += 1
+        if track_id > 81:
+            break
+
+capture.release()
+cv2.destroyAllWindows()
