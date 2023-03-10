847682c68a333e21be501a59f29f35d2bef7c63b
Ebihara Yusuke <yusuke@ebihara.me>
2023-03-11 07:05:32 +0900

    2ウィンドウで動画を表示

--- mktools_client/application.py
+++ mktools_client/application.py
@@ -4,6 +4,8 @@ from time import sleep
 import cv2
 from PIL import Image, ImageOps, ImageTk
 
+from .image.ta_result import detect_ta_result
+
 # windows dpi workaround
 try:
     import ctypes
@@ -28,16 +30,53 @@ class Application(tk.Frame):
 
         self.capture = cv2.VideoCapture(0)
 
+        cv2.CV_CAP_PROP_FPS = 60
+
         # create canvas
         self.canvas = tk.Canvas(master)
         self.canvas.pack(expand=True, fill=tk.BOTH)
-        
+
+        self.subwin = tk.Toplevel()
+        self.subwin.title("MKview")
+        self.subwin.state("zoomed")
+        self.sub_canvas = tk.Canvas(self.subwin)
+        self.sub_canvas.pack(expand=True, fill=tk.BOTH)
+
         self.video_loop()
 
     def video_loop(self):
-        _, frame = self.capture.read()
+        _, im = self.capture.read()
 
-        cv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        im_prev = im.copy()
+        detect_ta_result(im, im_prev)
+
+        self.draw_on_canvas(im_prev)
+        self.draw_on_sub_canvas(im)
+
+        self.video_loop_id = self.after(10, self.video_loop)
+
+    def draw_on_sub_canvas(self, im):
+        cv_image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        pil_image = Image.fromarray(cv_image)
+
+        canvas_width = self.sub_canvas.winfo_width()
+        canvas_height = self.sub_canvas.winfo_height()
+
+        # resize image
+        pil_image = ImageOps.pad(pil_image, (canvas_width, canvas_height))
+
+        # this must be stored to class to prevent from being destroied
+        self.photo_image_sub = ImageTk.PhotoImage(image=pil_image)
+
+        # 画像の描画
+        self.sub_canvas.create_image(
+            0, 0,
+            image=self.photo_image_sub,
+            anchor='nw'
+        )
+
+    def draw_on_canvas(self, im):
+        cv_image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
         pil_image = Image.fromarray(cv_image)
 
         canvas_width = self.canvas.winfo_width()
@@ -55,8 +94,6 @@ class Application(tk.Frame):
             image=self.photo_image,
             anchor='nw'
         )
-
-        self.video_loop_id = self.after(10, self.video_loop)
 
 def launch_app():
     win = tk.Tk()
