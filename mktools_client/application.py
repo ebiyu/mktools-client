f6c6a6b55164011fa0f52f62b7011e7485cee8f0
Ebihara Yusuke <yusuke@ebihara.me>
2023-03-11 07:26:30 +0900

    perf: スレッドで動画処理することで、負荷を軽減

--- mktools_client/application.py
+++ mktools_client/application.py
@@ -44,18 +44,21 @@ class Application(tk.Frame):
         self.sub_video_button = tk.Button(self.subwin)
         self.sub_video_button.pack(expand=True, fill=tk.BOTH)
 
-        self.video_loop()
+        self.video_thread = threading.Thread(target=self.video_loop)
+        self.video_thread.setDaemon(True)
+        self.video_thread.start()  
 
     def video_loop(self):
-        _, im = self.capture.read()
+        while True:
+            ret, im = self.capture.read()
+            if not ret:
+                continue
 
-        im_prev = im.copy()
-        detect_ta_result(im, im_prev)
+            im_prev = im.copy()
+            detect_ta_result(im, im_prev)
 
-        self.draw_on_canvas(im_prev)
-        self.draw_on_sub_canvas(im)
-
-        self.video_loop_id = self.after(10, self.video_loop)
+            self.draw_on_canvas(im_prev)
+            self.draw_on_sub_canvas(im)
 
     def draw_on_sub_canvas(self, im):
         cv_image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -67,10 +70,10 @@ class Application(tk.Frame):
         # resize image
         pil_image = ImageOps.pad(pil_image, (canvas_width, canvas_height))
 
+        photo_image = ImageTk.PhotoImage(image=pil_image)
+        self.sub_video_button.config(image=photo_image)
         # this must be stored to class to prevent from being destroied
-        self.photo_image_sub = ImageTk.PhotoImage(image=pil_image)
-
-        self.sub_video_button.config(image=self.photo_image_sub)
+        self.photo_image_sub = photo_image
 
     def draw_on_canvas(self, im):
         cv_image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -82,10 +85,10 @@ class Application(tk.Frame):
         # resize image
         pil_image = ImageOps.pad(pil_image, (canvas_width, canvas_height))
 
+        photo_image = ImageTk.PhotoImage(image=pil_image)
+        self.video_button.config(image=photo_image)
         # this must be stored to class to prevent from being destroied
-        self.photo_image = ImageTk.PhotoImage(image=pil_image)
-
-        self.video_button.config(image=self.photo_image)
+        self.photo_image = photo_image
 
 def launch_app():
     win = tk.Tk()
