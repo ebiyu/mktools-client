b75d2372b82c0fad2f57c33ca61954e7e6231209
Ebihara Yusuke <yusuke@ebihara.me>
2023-03-11 03:17:34 +0900

    edit entrypoint script

--- mktools_client/__main__.py
+++ mktools_client/__main__.py
@@ -2,19 +2,8 @@ import argparse
 from .auto_detect import start_auto_detect
 
 def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
-    parser_nita = subparsers.add_parser('nita')
-    parser_nita.set_defaults(handler=nita)
-
-    args = parser.parse_args()
-    if hasattr(args, 'handler'):
-        args.handler(args)
-    else:
-        parser.print_help()
-
-def nita(args):
     start_auto_detect()
+    return
 
 if __name__ == "__main__":
     main()
