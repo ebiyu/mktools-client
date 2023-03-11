6d9ab2aac3c450fa9f4336cfb9f8c235899e8992
Ebihara Yusuke <yusuke@ebihara.me>
2023-03-11 15:54:56 +0900

    ビデオキャプチャデバイスを選択できるようにした

--- mktools_client/application.py
+++ mktools_client/application.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 from time import sleep
 import threading
 
 import cv2
+import device
 from PIL import Image, ImageOps, ImageTk
 
 from .image.ta_result import detect_ta_result
@@ -28,12 +30,19 @@ class Application(tk.Frame):
 
         self.video_loop_id = None
 
-        self.capture = cv2.VideoCapture(0)
+        self.device_list = list(map(lambda x:x[0], device.getDeviceList()))
+        self.active_capture_index = None
+        self.capture = None
 
         cv2.CV_CAP_PROP_FPS = 60
 
         button_frm = tk.Frame(self, width=400)
         button_frm.pack(expand=False, fill=tk.Y, anchor=tk.E, side=tk.RIGHT)
+
+        self.video_text_var = tk.StringVar()
+        select = ttk.Combobox(self, textvariable=self.video_text_var, state="readonly", values=self.device_list)
+        select.bind("<<ComboboxSelected>>", self.on_device_change)
+        select.pack(padx=10, pady=10)
 
         button = tk.Button(button_frm, text="Open Preview", command=self.open_subwin)
         button.pack(padx=10, pady=10)
@@ -47,6 +56,21 @@ class Application(tk.Frame):
 
         self.running = True
         self.master.protocol("WM_DELETE_WINDOW", self.quit)
+
+    def on_device_change(self, *args, **kwargs):
+        # TODO: handle args
+        if self.video_text_var.get() not in self.device_list:
+            return
+
+        next_capture_index = self.device_list.index(self.video_text_var.get())
+        if self.active_capture_index == next_capture_index:
+            return
+
+        if self.capture is not None:
+            self.capture.release()
+
+        self.active_capture_index = next_capture_index
+        self.capture = cv2.VideoCapture(self.active_capture_index)
 
     def open_subwin(self):
         if self.sub_video_button is not None:
@@ -100,18 +124,22 @@ class Application(tk.Frame):
 
     def start(self):
         while self.running:
-            ret, im = self.capture.read()
-            if not ret:
-                continue
-
-            im_prev = im.copy()
-            detect_ta_result(im, im_prev)
-
-            self.draw_on_canvas(im_prev)
-            self.draw_on_sub_canvas(im)
+            self.update_video()
 
             self.update_idletasks()
             self.update()
+
+    def update_video(self):
+        if self.capture is None: return
+
+        ret, im = self.capture.read()
+        if not ret: return
+
+        im_prev = im.copy()
+        detect_ta_result(im, im_prev)
+
+        self.draw_on_canvas(im_prev)
+        self.draw_on_sub_canvas(im)
 
     def quit(self):
         self.running = False
