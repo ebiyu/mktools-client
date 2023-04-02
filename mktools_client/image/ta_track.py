e1ae0b571644e0769020cc8fddb57f37da9bda22
Ebihara Yusuke <yusuke@ebihara.me>
2023-04-02 13:32:09 +0900

    taの判定方式を変更

--- mktools_client/image/ta_track.py
+++ mktools_client/image/ta_track.py
@@ -11,50 +11,52 @@ def load_template_images():
         template_images.append(cv2.imread(str(Path(__file__).parent.parent / "data" /  f"tracks/{i}.png")))
     return template_images
 
+def get_ta_area_2bin(frame, inverse=False):
+    """
+    TAのコース名部分を切り出して二値化する
+    """
+    cropped = frame[950:1030, 500:1260, :]
+    cropped_gray= cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+    mode = cv2.THRESH_OTSU + (cv2.THRESH_BINARY_INV if inverse else cv2.THRESH_BINARY)
+    _, cropped_bin = cv2.threshold(cropped_gray, None, 255, mode)
+    return cropped_bin
+
 def detect_track(frame, prev_im=None):
     template_images = load_template_images()
 
-    im1 = template_images[1]
-    assert im1.shape[0] == 1080
-    assert im1.shape[1] == 1920
+    differences = [1] # それぞれのコースとどれだけ離れているか 1がmax
+    for track_id, im in enumerate(template_images):
+        if im is None:
+            continue
 
-        
-    # detect TA
-    im1_cropped = im1[940:1045, 500:1260, :]
-    frm_cropped = frame[940:1045, 500:1260, :]
-    diff = cv2.subtract(im1_cropped, frm_cropped)
-    diff_gray= cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    _, diff_bin = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
-    diff_bin_gray = cv2.cvtColor(diff_bin, cv2.COLOR_BGR2GRAY)
+        # 差分を計算してリストに格納する
+        frame_ta_area = get_ta_area_2bin(frame)
+        template_ta_area = get_ta_area_2bin(im)
+        diff = np.logical_xor(frame_ta_area, template_ta_area)
+        diff_rate = np.count_nonzero(diff) / diff.size
 
-    is_ta = np.count_nonzero(diff_bin_gray) < diff_bin_gray.size * 0.2
+        differences.append(diff_rate)
 
-    # 画像の表示
-    if is_ta:
-        differences = [1]
-        for i, im in enumerate(template_images):
-            if im is None:
-                continue
-            im_cropped = im[915:1045, 1280:1480, :]
-            frm_cropped = frame[915:1045, 1280:1480, :]
-            diff = cv2.subtract(im_cropped, frm_cropped)
-            diff_gray= cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-            _, diff_bin = cv2.threshold(diff, 3, 255, cv2.THRESH_BINARY)
-            differences.append(np.count_nonzero(diff_bin) / diff_bin.size)
+    result_difference = min(differences)
+    track_id = differences.index(result_difference)
+    is_ok = result_difference < 0.001
 
-        is_ok = min(differences) < 0.05
-        if is_ok:
-            i = differences.index(min(differences))
-            if prev_im is not None:
-                cv2.rectangle(prev_im, (1280, 915), (1480, 1045), (0, 0, 255), 5)
-                cv2.putText(prev_im, str(i), (1280, 915), 
-                fontFace=cv2.FONT_HERSHEY_TRIPLEX,
-                        fontScale=1.0,
-                        color=(0, 0, 255),
-                        thickness=2,
-                        lineType=cv2.LINE_4)
-            return i
-        else:
-            if prev_im is not None:
-                cv2.rectangle(prev_im, (1280, 915), (1480, 1045), (0, 0, 0), 5)
-            return None
+    if prev_im is not None:
+        color = (0, 0, 255) if is_ok else (128, 128, 128)
+        cv2.rectangle(prev_im, (500, 950), (1260, 1030), color, 5)
+        cv2.putText(
+            prev_im,
+            f'{track_id} (diff: {result_difference:.5f})',
+            (1280, 915), 
+            fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+            fontScale=1.0,
+            color=color,
+            thickness=2,
+            lineType=cv2.LINE_4,
+        )
+
+    # 正解判定の閾値
+    if is_ok:
+        return track_id
+    else:
+        return None
